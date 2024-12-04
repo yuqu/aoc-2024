5c1bfe9847cf536db77fe380ab45de6fa3352bc3
Yucel Okcu <yucel.n.okcu@gmail.com>
2024-12-04 18:24:36 +0300

    day1(java): optimize by sorting
    Signed-off-by: Yucel Okcu <yucel.n.okcu@gmail.com>

--- java/app/src/main/java/org/yuqu/Day1/Day1.java
+++ java/app/src/main/java/org/yuqu/Day1/Day1.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 
 public class Day1 {
@@ -14,63 +15,29 @@ public class Day1 {
     public static int solve(String filePath) throws IOException, URISyntaxException {
         var input = readInput(filePath);
 
+
+        Arrays.sort(input[0]);
+        Arrays.sort(input[1]);
+
         var total = 0;
-
         for (int i = 0; i < input[0].length; i++) {
-            var leftSmallestIndex = findSmallestIndexInArray(input[0]);
-            var rightSmallestIndex = findSmallestIndexInArray(input[1]);
-
-            total += Math.abs(input[0][leftSmallestIndex].getValue() - input[1][rightSmallestIndex].getValue());
-
-            input[0][leftSmallestIndex].setVisited();
-            input[1][rightSmallestIndex].setVisited();
+            total += Math.abs(input[0][i] - input[1][i]);
         }
 
         return total;
     }
 
-    private static int findSmallestIndexInArray(Value[] array) {
-        Integer current = null;
-        for (int i = 0; i < array.length; i++) {
-            if (!array[i].isVisited() && (current == null || array[i].getValue() < array[current].getValue())) {
-                current = i;
-            }
-        }
-        return current;
-    }
-
-    private static Value[][] readInput(String filePath) throws IOException, URISyntaxException {
+    private static int[][] readInput(String filePath) throws IOException, URISyntaxException {
         try (var fileStream = Files.lines(Paths.get(filePath))) {
             var lines = fileStream.toArray(String[]::new);
-            Value[][] input = new Value[2][lines.length];
+            int[][] input = new int[2][lines.length];
             for (int i = 0; i < lines.length; i++) {
                 var split = lines[i].split("   ");
-                input[0][i] = new Value(Integer.parseInt(split[0]));
-                input[1][i] = new Value(Integer.parseInt(split[1]));
+                input[0][i] = Integer.parseInt(split[0]);
+                input[1][i] = Integer.parseInt(split[1]);
             }
 
             return input;
-        }
-    }
-
-    private static class Value {
-        int value;
-        boolean visited = false;
-
-        public Value(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        public boolean isVisited() {
-            return visited;
-        }
-
-        public void setVisited() {
-            this.visited = true;
         }
     }
 }

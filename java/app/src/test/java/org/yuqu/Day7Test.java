a32acd8af1d46cdfb428bfeeef2da40ae8d488eb
Yucel Okcu <yucel.n.okcu@gmail.com>
2024-12-17 23:59:02 +0300

    day7(java): implement part1

---
+++ java/app/src/test/java/org/yuqu/Day7Test.java
@@ -0,0 +1,19 @@
+package org.yuqu;
+
+import org.junit.jupiter.api.Test;
+import org.yuqu.Day7.Day7;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Day7Test {
+    @Test
+    void test_part1() throws IOException, URISyntaxException {
+        var result = Day7.part1("src/test/resources/Day7/input.txt");
+
+        assertEquals(3749, result);
+    }
+}
+
